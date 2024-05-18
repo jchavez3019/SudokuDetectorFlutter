813f77eba3aa83b5bc3789b01eb394ebc4c636ba
jorgejc2 <jorgejc2@illinois.edu>
2024-05-17 19:47:30 -0500

    successfully trained a resnet model for digit classification

--- NumberModel.py
+++ NumberModel.py
@@ -4,42 +4,140 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.downsample = None
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
 class NumberModel(nn.Module):
     def __init__(self, lrate, loss_fn, in_size, out_size):
+        """
+        Initializes the Neural Network model
+        @param lrate:
+        @param loss_fn:
+        @param in_size:
+        @param out_size:
+        """
         super(NumberModel, self).__init__()
 
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels=in_size, out_channels=96, kernel_size=(11, 11), stride=4, padding="valid"),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
-            nn.Conv2d(96, 256, kernel_size=(5, 5), stride=1, padding=2),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
-            nn.Conv2d(256, 384, kernel_size=(3, 3), stride=1, padding=1),
-            nn.Conv2d(384, 384, kernel_size=(3, 3), stride=1, padding=1),
-            nn.Conv2d(384, 256, kernel_size=(3, 3), stride=1, padding=1),
-            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
-            nn.Flatten(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.Linear(4096, 4096),
-            nn.Linear(4096, 1000),
-            nn.Linear(1000, out_size),
-            nn.ReLU(),
-        )
+        # self.conv_layers = nn.Sequential(
+        #     nn.Conv2d(in_channels=in_size, out_channels=96, kernel_size=(11, 11), stride=4, padding="valid"),
+        #     nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+        #     nn.Conv2d(96, 256, kernel_size=(5, 5), stride=1, padding=2),
+        #     nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+        #     nn.Conv2d(256, 384, kernel_size=(3, 3), stride=1, padding=1),
+        #     nn.Conv2d(384, 384, kernel_size=(3, 3), stride=1, padding=1),
+        #     nn.Conv2d(384, 256, kernel_size=(3, 3), stride=1, padding=1),
+        #     nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+        # )
+        # self.linear_layers = nn.Sequential(
+        #     nn.Flatten(),
+        #     nn.Linear(256 * 6 * 6, 4096),
+        #     nn.ReLU(),
+        #     nn.Linear(4096, 4096),
+        #     nn.ReLU(),
+        #     nn.Linear(4096, 1000),
+        #     nn.ReLU(),
+        #     nn.Linear(1000, out_size),
+        #     nn.ReLU()
+        # )
+        block = BasicBlock
+        layers = [2,2,2,2]
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, out_size)
 
         self.loss_fn = loss_fn
         self.optimizer = optim.SGD(self.parameters(), lr=lrate, weight_decay=1e-5)
 
-    def forward(self, x):
-        return self.model(x)
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the model
+        @param x:
+        @return:
+        """
+        # y = self.conv_layers(x)
+        # return self.linear_layers(y)
+        # return self.model(x)
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
     
-    def step(self, x, y):
+    def step(self, x: torch.Tensor, y):
+        """
+        Returns the loss of a single forward pass
+        @param x:
+        @param y:
+        @return:
+        """
         self.optimizer.zero_grad()
 
         # pass the batch through the model
-        y_hat = self.model(x)
+        y_hat = self.forward(x)
 
         # compute the loss
-        loss = self.loss_fn(y_hat, y)
+        loss = self.loss_fn(y_hat, y.to(dtype=torch.long))
 
         # update model
         loss.backward()
@@ -50,11 +148,11 @@ class NumberModel(nn.Module):
 class SudokuDataset(Dataset):
     """Face Landmarks dataset."""
 
-    def __init__(self, X,y):
+    def __init__(self, X: np.ndarray,y: np.ndarray):
         """
-        Args:
-            X [np.array]: features vector
-            y [np.array]: labels vector          
+        Initializes the dataset
+        @param X: features vector
+        @param y: labels vector
         """
         self.data = X
         self.labels = y
@@ -76,16 +174,21 @@ def fit(train_dataset, test_dataset, image_dim, epochs, batch_size=50):
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
-    NetObject = NumberModel(0.01, nn.CrossEntropyLoss(), image_dim[0], 10)
+    NetObject = NumberModel(0.01, nn.CrossEntropyLoss(), image_dim[0], 10).to(device)
 
     losses = []
+    epoch_progress_bar = tqdm(range(epochs), desc="Epochs", leave=True)
     for epoch in range(epochs):
-        print(f"\rEpoch {epoch}", end="")
         epoch_loss = 0.0
         for batch in train_loader:
             batch_x, batch_y = batch
-            epoch_loss += NetObject.step(batch[batch_x], batch[batch_y])
+            batch_x = batch_x.to(device)
+            batch_y = batch_y.to(device)
+            epoch_loss += NetObject.step(batch_x, batch_y)
         epoch_loss /= len(train_loader)
+        epoch_progress_bar.update(1)
+        epoch_progress_bar.set_postfix({'Epoch Loss': epoch_loss})
+    print("\n")
 
     # Set the model to evaluation mode
     NetObject.eval()
@@ -98,14 +201,14 @@ def fit(train_dataset, test_dataset, image_dim, epochs, batch_size=50):
     # Iterate through the test DataLoader
     for images, labels in test_loader:
         # Move images and labels to the device the model is on (e.g., GPU)
-        # images = images.to(device)
-        # labels = labels.to(device)
+        images = images.to(device)
+        labels = labels.to(device)
 
         # Forward pass
         with torch.no_grad():  # No need to compute gradients during inference
             outputs = NetObject(images)
 
-        # Get predicted labels
+        # Get predicted labels (and ignore the values themselves)
         _, predicted = torch.max(outputs, 1)
 
         # Update the total number of examples
@@ -119,3 +222,9 @@ def fit(train_dataset, test_dataset, image_dim, epochs, batch_size=50):
     print('Accuracy:', accuracy)
 
     return losses, NetObject
+
+
+if __name__ == "__main__":
+    from python_helper_functions import get_sudoku_dataset
+    train_data, test_data, single_image_dimension = get_sudoku_dataset()
+    losses, NetObject = fit(train_data, test_data, single_image_dimension, 20, 50)
